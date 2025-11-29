91982bbc14d8dd2089b37df6811c5a3f0c371d8e
Cristóbal <crfigueroa2024@udec.cl>
2025-11-29 20:46:27 -0300

    feat: implementar simulador que lee las lineas de un txt con manejo de archivos y estadísticas de acceso

--- act2/sim.c
+++ act2/sim.c
@@ -1,11 +1,83 @@
 #include "sim.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <descomponer.h>
 #include <obtener_direccion_fisica.h>
 
 
-void simulador(){
+void simulador(const char *archivo_traza, int n_marcos, int tam_marco, int verbose){
+    FILE *fp = fopen(archivo_traza, "r");
+    if (fp == NULL){
+        fprintf(stderr, "Error. No se puede abrir el archivo de traza '%s'\n", archivo_traza);
+        __EXIT(1);
+    }
+
+    int page_bits = calcular_page_bits(tam_marco);
+
+    int num_paginas_virtuales = (1 << (32 - page_bits)); // Asumimos direcciones de 32 bits
+
+    PageTableEntry *tabla_paginas;
+    Marco *memoria_fisica;
+
+    inicializar_estructuras(&tabla_paginas, &memoria_fisica, n_marcos, num_paginas_virtuales);
+
     int puntero_reloj = 0;
+    int total_fallos = 0; 
+    int total_accesos = 0;
+
+    char linea[256];
+
+    if (verbose)
+    {
+        printf("===========================\n");
+        printf("Marcos físicos: %d\n", n_marcos);
+        printf("Tamaño de marco: %d\n", tam_marco);
+        printf("Bits de offset: %d\n", page_bits);
+        printf("===========================\n");
+    }
+
+    while (fgets(linea, sizeof(linea), fp) !=NULL)
+    {
+        // Elimina salto de linea
+        linea[strcspn(linea, "\r\n")] = 0;
+
+        unsigned int dv = parsear_direccion(linea);
+        int fallo;
+
+        total_accesos++;
+        if (verbose) {
+            printf("Acceso #%d: DV = %u (0x%X)\n", total_accesos, dv, dv);
+        }
+
+        unsigned int df = traducir_direccion(dv, tabla_paginas, memoria_fisica, n_marcos,
+                                             page_bits, &puntero_reloj, &fallo, verbose);
+        
+        if (fallo)
+        {
+            total_fallos++;
+        }
+        
+        if (verbose)
+        {
+            unsigned int nvp, offset;
+            descomponer(dv, &nvp, &offset, page_bits);
+            printf("NVP = %u, offset = %u\n", nvp, offset);
+            printf("Direccion física = %u, (0x%X)", df, df);
+            printf("%s\n\n", fallo ? "FALLO DE PÁGINA" : "HIT");
+        }    
+    }
+    
+    printf("\n===RESULTADOS===");
+    printf("Total de accesos: %d \n", total_accesos);
+    printf("Fallos de página: %d \n", total_fallos);
+    printf("Tasa de fallos: %.2f%%\n", (double)total_fallos / total_fallos * 100);
+    printf("=================\n");
+
+    fclose(fp);
+    free(tabla_paginas);
+    free(memoria_fisica);
+
+
 }
 
 int buscar_marco_libre(Marco *memoria_fisica, int n_marcos){
@@ -105,4 +177,16 @@ int calcular_page_bits(int tam_marco){
         bits++;
     }
     return bits;
+}
+
+unsigned int parsear_direccion(const char *str){
+    unsigned int dv;
+    if (strncmp(str, "0x", 2) == 0)
+    {
+        sscanf(str, "%x", &dv);
+    } else
+    {
+        sscanf(str, "%u", &dv);
+    }
+    return dv;
 }
