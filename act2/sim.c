f05ae09e34f45ebbf783ac9ea32920f63155a19d
Cristóbal <crfigueroa2024@udec.cl>
2025-11-29 20:08:50 -0300

    feat: agregar método para traducir direcciones que maneja hits y page faults dentro de la tabla de paginas

--- act2/sim.c
+++ act2/sim.c
@@ -37,3 +37,66 @@ int algoritmo_reloj(Marco *memoria_fisica, int n_marcos, int *puntero_reloj){
         *puntero_reloj = (*puntero_reloj + 1) % n_marcos;
     }
 }
+
+unsigned int traducir_direccion(unsigned int dv, PageTableEntry *tabla_paginas, Marco *memoria_fisica,
+                                int n_marcos, int page_bits, int *puntero_reloj, int *fallo, int verbose)
+    {
+    unsigned int nvp, offset;
+
+    descomponer(dv, &nvp ,&offset, page_bits);
+
+    *fallo = 0;
+    int marco;
+
+    // hit
+    if(tabla_paginas[nvp].valid = 1){
+        marco = tabla_paginas[nvp].marco_fisico;
+        memoria_fisica[marco].bit_referencia = 1;
+        tabla_paginas[nvp].referencia = 1;
+        if (verbose)
+        {
+            printf("HIT : Página %u ya está en marco %d \n", nvp, marco);
+        }
+        
+    } else{ // page fault
+        *fallo = 1;
+        marco = buscar_marco_libre(memoria_fisica, n_marcos);
+
+        if (marco != -1) { // hay marco libre
+            if (verbose)
+            {
+                printf("FALLO: Página %u no está en memoria. Asignando marco %d \n", nvp, marco);
+            }
+        } else { 
+            marco = algoritmo_reloj(memoria_fisica, n_marcos, puntero_reloj);
+            int pagina_victima = memoria_fisica[marco].pagina_virtual;
+
+            if (verbose)
+            {
+                printf("FALLO: Página %u no está en memoria. Reemplazando página %d en marco %d \n",
+                nvp, pagina_victima, marco);
+            }
+            
+            // Invalidar la página victima en la tabla de páginas
+            if (pagina_victima >= 0) 
+            {
+                tabla_paginas[pagina_victima].valid = 0;
+                tabla_paginas[pagina_victima].marco_fisico = -1;
+        
+            }
+        }
+        
+        // Se asigna el marco a la nueva pagina
+        memoria_fisica[marco].pagina_virtual = nvp;
+        memoria_fisica[marco].bit_referencia = 1;
+
+        tabla_paginas[nvp].marco_fisico = marco;
+        tabla_paginas[nvp].valid = -1;
+        tabla_paginas[nvp].referencia = 1;
+    } 
+
+    unsigned int df = obtener_direccion_fisica(marco, offset, page_bits);
+
+    return df;
+
+}
